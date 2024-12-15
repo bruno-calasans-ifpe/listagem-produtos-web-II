339b7de0941a00ccb547ced2673b66729c203055
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2024-12-15 10:09:18 -0300

    add infinite scroll to products

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,15 +1,13 @@
-import useProducts from "../hooks/useProducts";
+import useProducts from "../hooks/UseProducts";
 import ProductCard from "./ProductCard";
 import ProductError from "./ProductError";
 import Loader from "./Loader";
 import Center from "./Center";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-type ProductsListProps = {
-  //   products: Product[];
-};
-
-export default function ProductsList({}: ProductsListProps) {
-  const { products, loading, error } = useProducts();
+export default function ProductsList() {
+  const { products, error, pagination, loadProducts } = useProducts();
+  console.log(products);
 
   if (error)
     return (
@@ -21,19 +19,26 @@ export default function ProductsList({}: ProductsListProps) {
       </Center>
     );
 
-  if (loading) {
-    return (
-      <Center>
-        <Loader text="Carregando produtos..." />;
-      </Center>
-    );
-  }
-
   return (
-    <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-5">
-      {products.map((p) => (
-        <ProductCard key={p.title} product={p} />
+    <InfiniteScroll
+      dataLength={products.length}
+      next={loadProducts}
+      hasMore={pagination.hasMore}
+      loader={
+        <div className="col-span-3">
+          <Loader />
+        </div>
+      }
+      endMessage={
+        <div className="flex flex-1 items-center justify-center col-span-3 text-lg text-stone-400 font-bold">
+          Você chegou ao final da lista
+        </div>
+      }
+      className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-5 p-2"
+    >
+      {products.map((p, i) => (
+        <ProductCard key={p.title + i} product={p} />
       ))}
-    </div>
+    </InfiniteScroll>
   );
 }
--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -3,21 +3,40 @@ import { Product } from "../types/Product.type";
 import api from "../lib/axios/axios";
 import { AxiosError } from "axios";
 
+type Pagination = {
+  offset: number;
+  limit: number;
+  hasMore: boolean;
+};
+
 export default function UseProducts() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<AxiosError | null>(null);
+  const [pagination, setPagination] = useState<Pagination>({
+    offset: 0,
+    limit: 10,
+    hasMore: true,
+  });
 
-  const loadProducts = async (offset = 0, limit = 10) => {
+  const loadProducts = async () => {
     setLoading(true);
+    const { offset, limit } = pagination;
     try {
-      const { data } = await api.get<Product[]>("/", {
+      const { data } = await api.get<Product[]>("", {
         params: {
           offset,
           limit,
         },
       });
-      setProducts(data);
+
+      const cleanupData = data.filter((p) => p.title != "New Product");
+      setProducts((current) => [...current, ...cleanupData]);
+      setPagination((current) => ({
+        ...current,
+        offset: current.offset + limit,
+        hasMore: cleanupData.length > 0,
+      }));
     } catch (error) {
       if (error instanceof AxiosError) {
         setError(error);
@@ -31,5 +50,5 @@ export default function UseProducts() {
     loadProducts();
   }, []);
 
-  return { products, loading, error };
+  return { products, loading, error, pagination, loadProducts };
 }
